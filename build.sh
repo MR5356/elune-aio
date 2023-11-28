daf67f0e12b2cab27ba17cca40c10b3d9f6d0e95
Rui Ma <535657150@qq.com>
2023-11-28 14:28:07 +0800

    增加二进制构建

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 set -x
-VERSION=v1.0.3
+BRANCH=main
+VERSION=v1.0.4
 
 rm -rf out
 
 echo "准备代码"
-git clone -b ${VERSION} --depth=1 git@github.com:MR5356/Elune.git elune
-git clone -b ${VERSION} --depth=1 git@github.com:MR5356/elune-backend.git elune-backend
+git clone -b ${BRANCH} --depth=1 git@github.com:MR5356/Elune.git elune
+git clone -b ${BRANCH} --depth=1 git@github.com:MR5356/elune-backend.git elune-backend
 
 echo "构建镜像"
 docker buildx build --platform linux/arm64,linux/amd64 -t registry.cn-hangzhou.aliyuncs.com/toodo/elune-aio:${VERSION} . --push
 mkdir -p out
-docker buildx build --platform linux/arm64,linux/amd64 --output type=local,dest=out .
+docker buildx build -f Dockerfile-bin --output type=local,dest=out .
 
 echo "清理目录"
 rm -rf elune
